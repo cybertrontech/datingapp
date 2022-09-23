331f5d86530181240e8c6fd2ef7e042fd712aa47
Yugal Pariyar <yugalpariyar@Yugals-MacBook-Air.local>
2022-09-23 21:17:36 +0545

    added changes

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,14 +1,12 @@
-import 'package:datingapp/screen/home/navigation_button_screen/components/nav_button_bar.dart';
+// import 'package:datingapp/screen/home/navigation_button_screen/components/nav_button_bar.dart';
 import 'package:flutter/material.dart';
-
-
 
 class HomeScreen extends StatelessWidget {
   static String routeName = "/home";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Navigationbar(),
-    );
+        // body: Navigationbar(),
+        );
   }
 }
